1714d5dfb05b4b7bdb5e7d85618443e8fe802ed8
Maksym Teplyy <mate@ciklum.com>
2018-07-30 11:59:40 +0300

    Apply valid gradation sorting

--- src/components/Gradation.jsx
+++ src/components/Gradation.jsx
@@ -5,21 +5,23 @@ import 'components/Gradation.css';
 
 class Gradation extends Component {
   get placesList() {
-    return this.props.places.map((place, index) =>
-      <div className="place" key={index}>
-        <div className="place-rating">{index + 1}:</div>
-        <div className="place-data">
-          <div className="place-rate">
-            <IoConnectbars size={24} style={{ marginRight: 7.5 }} />
-            Total Rate: {place.totalRate}
-          </div>
-          <div className="place-coords">
-            <div className="place-coords-header"><IoMap size={18} style={{ marginRight: 5 }} /> Coordinates:</div>
-            <div className="place-lat">Latitude: {place.lat}</div>
-            <div className="place-lng">Longitude: {place.lng}</div>
+    return this.props.places
+      .sort((place1, place2) => place1.totalRate + place2.totalRate)
+      .map((place, index) =>
+        <div className="place" key={index}>
+          <div className="place-rating">{index + 1}:</div>
+          <div className="place-data">
+            <div className="place-rate">
+              <IoConnectbars size={24} style={{ marginRight: 7.5 }} />
+              Total Rate: {place.totalRate}
+            </div>
+            <div className="place-coords">
+              <div className="place-coords-header"><IoMap size={18} style={{ marginRight: 5 }} /> Coordinates:</div>
+              <div className="place-lat">Latitude: {place.lat}</div>
+              <div className="place-lng">Longitude: {place.lng}</div>
+            </div>
           </div>
         </div>
-      </div>
     );
   }
 
